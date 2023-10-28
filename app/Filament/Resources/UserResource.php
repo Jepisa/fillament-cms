b70f479da80589aa71ca76274629fbb6096d928d
Jean Piere Sanchez <javierjeanpieres@gmail.com>
2023-10-28 04:45:53 -0300

    add - plugin Excel Export

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -16,6 +16,7 @@ use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class UserResource extends Resource
 {
@@ -99,6 +100,7 @@ class UserResource extends Resource
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
+                    ExportBulkAction::make(),
                 ]),
             ])
             ->defaultSort('name', 'desc');
